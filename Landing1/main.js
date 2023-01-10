268ab5c44c8a72a35243e3fd76caa76f62cec6f9
TotSamyiDanya <TotSamyiDanya@yandex.ru>
2023-01-10 22:49:44 +0300

    add switch

---
+++ Landing1/main.js
@@ -0,0 +1,21 @@
+document.getElementById("ulBtn").onclick = function () {
+	ul = document.getElementById('switchUlStyle');
+	v = window.getComputedStyle(ul).visibility;
+	if (v == "visible") {
+	}
+	else {
+		document.getElementById("switchUlStyle").classList.toggle("visible");
+		document.getElementById("switchGridStyle").classList.toggle("invisible");
+	}
+}
+
+document.getElementById('gridBtn').onclick = function () {
+	grid = document.getElementById('switchGridStyle');
+	v = window.getComputedStyle(grid).visibility;
+	if (v == "visible") {
+	}
+	else {
+		document.getElementById("switchGridStyle").classList.toggle("invisible");
+		document.getElementById("switchUlStyle").classList.toggle("visible");
+	}
+}
